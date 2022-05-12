6e7ba3319d889d85c13b241ab77ecdbc92944bb2
kaym0 <kevinrogermurphy@gmail.com>
2022-05-12 17:19:33 -0230

    Casing

--- components/eyeFollow.tsx
+++ components/eyeFollow.tsx
@@ -1,9 +1,8 @@
 import { useEffect, useState } from "react";
-
 import Lottie from "react-lottie";
 import { Parallax } from "react-scroll-parallax";
 import styles from "../styles/skullFollow.module.scss"
-import skullAnim from "../animations/skullAnimWhite.json"
+import skullAnim from "../animations/SkullAnimWhite.json"
 
 
 export default function EyeFollow() {
