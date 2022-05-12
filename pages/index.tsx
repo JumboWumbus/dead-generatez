e33c9697fe5db1157a14421f1e0793d5d67ee207
kaym0 <kevinrogermurphy@gmail.com>
2022-05-12 17:39:42 -0230

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import DeadgeneratezTypeLogo from "../components/deadgeneratezTypeLogo";
+import DeadgeneratezTypeLogo from "../components/svg/deadgeneratezTypeLogo";
 import EyeFollow from "../components/eyeFollow";
 import NavBar from "../components/navBar";
 import TeamSection from "../components/teamSection";
