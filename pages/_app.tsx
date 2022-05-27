d27d26d4399c89e7e82922415f97e2fb80ae8c30
JumboWumbus <stepabovetheeggs@protonmail.com>
2022-05-27 13:58:43 +0100

    added prelim GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,36 @@
-import { AnimatePresence, motion } from "framer-motion";
-import { AppProps } from "next/app";
-import "../styles/globals.scss";
-import "../styles/fonts.scss";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { AnimatePresence, motion } from 'framer-motion';
+import { AppProps } from 'next/app';
+import '../styles/globals.scss';
+import '../styles/fonts.scss';
+import { useRouter, withRouter } from 'next/router';
+import { useEffect } from 'react';
+
+import ReactGA from 'react-ga';
+const TRACKING_ID = 'UA-230128226-1';
+
+ReactGA.initialize(TRACKING_ID);
 
 function MyApp({ Component, pageProps }: AppProps) {
-    const router = useRouter();
+  const router = useRouter();
 
-    
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  });
 
-    
-    return (
-        <>
-            <AnimatePresence exitBeforeEnter>
-                <motion.div initial="initial" animate="animate" exit="exit" key={router.route}>
-                    <Component {...pageProps} key={router.route} />
-                </motion.div>
-            </AnimatePresence>
-        </>
-    );
+  return (
+    <>
+      <AnimatePresence exitBeforeEnter>
+        <motion.div
+          initial='initial'
+          animate='animate'
+          exit='exit'
+          key={router.route}
+        >
+          <Component {...pageProps} key={router.route} />
+        </motion.div>
+      </AnimatePresence>
+    </>
+  );
 }
 
-export default MyApp;
+export default withRouter(MyApp);
